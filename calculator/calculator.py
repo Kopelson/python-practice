8baae7d9bb0ba92a5ee9d11344460273a139e4ca
Ken <kennethkopelson@gmail.com>
2021-12-17 16:17:56 -0800

    updated calculator to except operators

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,5 +1,14 @@
-num1 = input("Enter a number: ")
-num2 = input("Enter another number: ")
-result = float(num1) + float(num2)
+num1 = float(input("Enter a number: "))
+operator = input("Enter an operator: ")
+num2 = float(input("Enter another number: "))
 
-print(result)
+if operator == "+":
+    print(num1 + num2)
+elif operator == "-":
+    print(num1 - num2)
+elif operator == "/":
+    print(num1 / num2)
+elif operator == "*":
+    print(num1 * num2)
+else:
+    print("Invalid operator")
